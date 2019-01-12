ad5b0ab705ab90ea32c22019f00f8bab14615a38
nverba <distantstate@gmail.com>
2019-01-12 15:32:28 +0000

    save demo mock json data

--- demos/src/router.js
+++ demos/src/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
-import RestaurantBookingDemo from './views/RestaurantBookingDemo/RestaurantBookingDemo'
+import Home from './demos/Home.vue'
+import RestaurantBookingDemo from './demos/RestaurantBookingDemo/RestaurantBookingDemo'
 
 Vue.use(Router)
 
