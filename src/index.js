2ae32ec411d313b800a252213afc01e89b817e97
nverba <distantstate@gmail.com>
2019-07-24 14:50:06 +0100

    Move types to separate resource

--- src/index.js
+++ src/index.js
@@ -2,5 +2,3 @@ export { createFlow } from './create-flow.js';
 export { reduxStoreMiddleware } from './middleware/redux-store/redux-store-middleware.js';
 export { reduceMiddleware } from './middleware/reduce/reduce-middleware.js';
 export { wait, resolveWait, removeWait } from './functions/wait/promise-wait-function';
-
-export { Flow } from './types/flow.ts'
