ced584c6cacad3ca8083dba8d4d6f56aa99932d9
nverba <distantstate@gmail.com>
2019-07-24 16:12:51 +0100

    Create new wait middleware

--- src/functions/wait/promise-wait-middleware.js
+++ src/functions/wait/promise-wait-middleware.js
@@ -4,23 +4,29 @@ export const promiseWaitMiddleware = () => {
 
   const promiseStore = createWait();
 
-  return next => directive => {
+  const middleware = next => directive => {
 
     const result = next(directive);
 
-    if (directive.waitOn) {
-      return { call: promiseStore.wait, args: { on: directive.waitOn, async: directive.async }};
+    if (directive.wait) {
+      return { call: promiseStore.wait, args: directive.wait };
     }
 
-    if (directive.resolveWaitOn) {
-      return { call: promiseStore.resolveWait, args: { on: directive.on, async: directive.async }};
+    if (directive.resolveWait) {
+      return { call: promiseStore.resolveWait, args: directive.resolveWaitOn };
     }
 
-    if (directive.removeWaitFor) {
-      return { call: promiseStore.removeWait, args: { on: directive.on, async: directive.async }};
+    if (directive.removeWait) {
+      return { call: promiseStore.removeWait, args: directive.removeWait};
     }
 
     return result;
   
   };
+
+  middleware.wait = promiseStore.wait
+  middleware.resolveWait = promiseStore.resolveWait;
+  middleware.removeWait = promiseStore.removeWait;
+
+  return middleware;
 };
