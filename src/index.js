eb3703211441ccd4cd5d439486876a962d51bf53
nverba <distantstate@gmail.com>
2019-07-24 11:57:28 +0100

    Export other assets

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-export { createFlow } from './create-flow.js' 
+export { createFlow } from './create-flow.js';
+export { reduxStoreMiddleware } from './middleware/redux-store/redux-store-middleware.js';
+export { reduceMiddleware } from './middleware/reduce/reduce-middleware.js';
+export { wait, resolveWait, removeWait } from './functions/wait/promise-wait-function';
