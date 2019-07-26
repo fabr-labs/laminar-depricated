57c5377507398feceb7fd877ff904af8d71a9e8d
nverba <distantstate@gmail.com>
2019-07-26 11:49:08 +0100

    Clean up

--- src/middleware/save-response/save-response-middleware.js
+++ src/middleware/save-response/save-response-middleware.js
@@ -3,20 +3,9 @@ export const saveResponseMiddleware = () => {
   const responseStore = new Map();
 
   const middleware = next => directive => {
-
-
-    if (directive.useResponse) {
-      console.log(directive.useResponse);
-      console.log(responseStore.get(directive.useResponse));
-      console.log({ ...directive, args: { ...(directive.args || {}), [directive.useResponse]: responseStore.get(directive.useResponse) } })
-    }
-
     const result = next(directive.useResponse ? { ...directive, args: { [directive.useResponse]: responseStore.get(directive.useResponse), ...(directive.args || {}) } } : directive);
   
     if (directive.saveResponse) {
-
-      console.log(directive)
-
       if (result.then) {
         result.then(response => {
           responseStore.set(directive.saveResponse, response)
