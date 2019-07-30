98214ee250b41674ac6f0aad0256e8d058d8708c
nverba <distantstate@gmail.com>
2019-07-30 15:58:59 +0100

    Export middleware

--- src/index.js
+++ src/index.js
@@ -4,3 +4,4 @@ export { reduceMiddleware } from './middleware/reduce/reduce-middleware.js';
 export { waitMiddleware } from './middleware/wait/wait-middleware.js';
 export { pushFlowMiddleware } from './middleware/push-flow/push-flow-middleware.js';
 export { saveResponseMiddleware } from './middleware/save-response/save-response-middleware.js';
+export { fetchMiddleware } from './middleware/fetch/fetch-middleware.js';
