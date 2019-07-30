39acf50df61406b837b4aa419a1c4b98623a25ab
nverba <distantstate@gmail.com>
2019-07-30 15:57:01 +0100

    Create fetch middleware

---
+++ src/middleware/fetch/fetch-middleware.js
@@ -0,0 +1,20 @@
+export const reduxStoreMiddleware = ({ get, put, post, delete: del }) => next => directive => {
+
+  if (directive.get) {
+    return { call: get, args: directive.get, ...directive }
+  }
+
+  if (directive.put) {
+    return { call: put, args: directive.put, ...directive }
+  }
+
+  if (directive.post) {
+    return { call: post, args: directive.post, ...directive }
+  }
+
+  if (directive.delete) {
+    return { call: del, args: directive.delete, ...directive }
+  }
+
+  return  next(directive);
+};
