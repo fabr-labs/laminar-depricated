dbd5cc4d8c18385aaa5410c169b2377c60c5051b
nverba <distantstate@gmail.com>
2019-07-30 16:22:18 +0100

    Forgot to call nect with middleware

--- src/middleware/fetch/fetch-middleware.js
+++ src/middleware/fetch/fetch-middleware.js
@@ -1,19 +1,19 @@
 export const fetchMiddleware = ({ get, put, post, delete: del }) => next => directive => {
 
   if (directive.get) {
-    return { call: get, args: directive.get, ...directive }
+    return next({ call: get, args: directive.get, ...directive });
   }
 
   if (directive.put) {
-    return { call: put, args: directive.put, ...directive }
+    return next({ call: put, args: directive.put, ...directive });
   }
 
   if (directive.post) {
-    return { call: post, args: directive.post, ...directive }
+    return next({ call: post, args: directive.post, ...directive });
   }
 
   if (directive.delete) {
-    return { call: del, args: directive.delete, ...directive }
+    return next({ call: del, args: directive.delete, ...directive });
   }
 
   return  next(directive);
