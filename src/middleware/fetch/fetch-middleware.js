e58ab084b3f00e69d0700f8768a52827c93ecc6a
nverba <distantstate@gmail.com>
2019-08-05 10:49:26 +0100

    Fixup fetch middleware

--- src/middleware/fetch/fetch-middleware.js
+++ src/middleware/fetch/fetch-middleware.js
@@ -1,19 +1,19 @@
 export const fetchMiddleware = ({ get, put, post, delete: del }) => next => ({ directive, meta }) => {
 
   if (directive.get) {
-    return next({ call: get, args: directive.get, ...directive });
+    return next({ directive: { call: get, args: directive.get, ...directive }, meta });
   }
 
   if (directive.put) {
-    return next({ call: put, args: directive.put, ...directive });
+    return next({ directive: { call: put, args: directive.put, ...directive }, meta });
   }
 
   if (directive.post) {
-    return next({ call: post, args: directive.post, ...directive });
+    return next({ directive: { call: post, args: directive.post, ...directive }, meta });
   }
 
   if (directive.delete) {
-    return next({ call: del, args: directive.delete, ...directive });
+    return next({ directive: { call: del, args: directive.delete, ...directive }, meta });
   }
 
   return  next({ directive, meta });
