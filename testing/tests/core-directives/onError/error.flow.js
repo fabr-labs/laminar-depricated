05d4fd388b7849f612e62c33d551c33d4a78aad9
nverba <distantstate@gmail.com>
2020-01-08 09:54:41 +0000

    Add tests for error flows

--- testing/tests/core-directives/onError/error.flow.js
+++
@@ -1,9 +0,0 @@
-import { testFn } from "../../../helpers/test-fn.js";
-
-export function errorFlow() { 
-  return [
-    { id: "id1", call: testFn, args: 'step-1'},
-    { id: "id2", args: 'step-2' },
-    { id: "id3", call: testFn, args: 'step-3' }
-  ];
-}
